0972c4a249d8bbbdc0956923887e43e85c8adb15
sgmiriuka <sgmiriuka@gmail.com>
2022-01-29 10:58:06 -0300

    change to tweet with title instead of keywords

--- scripts/biorxivbot.py
+++ scripts/biorxivbot.py
@@ -19,27 +19,29 @@ def search_and_tweet():
           api = tweet_login()
           dois = []
           for n_tweets, line in enumerate(k_now):
-               matched_kw = line[0]
+               # matched_kw = line[0] # All commented lines are for tweeting with keywords
                tu = list(line[1])
                doi = tu[0]
-               title = tu[1]  #not used so far
+               matched_title = tu[1]
                version = tu[2]
                if doi in dois:
                     continue
                else:
                     dois.append(doi)
                link = "https://www.biorxiv.org/content/" + doi +'v' + version
-               n_char = len(matched_kw)
-               message = 'Keywords'
-               if n_char > 117:
-                    matched_kw_length = 117 - len(message)
-                    _matched_kw = matched_kw[:matched_kw_length]
-                    final_matched_kw = _matched_kw + '...'
+               n_char = len(matched_title)
+               #message = 'Keywords'
+               if n_char > 124:   #change this number to 117 if keywords
+                    #matched_kw_length = 117 - len(message)
+                    matched_title_length = 124
+                    _matched_title = matched_title[:matched_title_length]
+                    final_matched_title = _matched_title + '...'
                else:
-                    final_matched_kw = matched_kw
+                    final_matched_title = matched_title
                cwd = os.getcwd()
                with open(cwd + '/bioRxivbot/scripts/temp.txt', 'w') as f:
-                    f.write(message + '\n' + final_matched_kw + '\n' + link)
+                    #f.write(message + '\n' + final_matched_kw + '\n' + link)
+                    f.write(final_matched_title + '\n' + link)
                with open(cwd + '/bioRxivbot/scripts/temp.txt', 'r') as f:
                          api.update_status(f.read())
                f.close()
